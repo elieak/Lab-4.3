8d9a793700ef5e6028a387784248e7be845dc928
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-31 21:02:48 +0300

    4.3

--- Primes/Program.cs
+++ Primes/Program.cs
@@ -12,14 +12,26 @@ namespace Primes
             for (var i = a; i <= b; i++)
             {
                 var isPrime = true;
+
+                //The conveintion in C# is to start body expressions in a seperate line and with bracets "{ }", even for oneliners
                 if ((i & 1) == 0)
+                {
                     if (i != 2)
+                    {
                         isPrime = false;
+                    }
+                }
                 for (var j = 3; (j*j) <= i; j += 2)
+                {
                     if (i%j == 0)
+                    {
                         isPrime = false;
+                    }
+                }
                 if (isPrime)
+                {
                     arrayList.Add(i);
+                }
             }
 
             var myPrimesArray = new int[arrayList.Count];
@@ -29,10 +41,13 @@ namespace Primes
         }
         static void Main()
         {
-            const int fn = 0;
-            const int sn = 100;
+
+            //consider better names
+            const int fn = 3;
+            const int sn = 20;
             var calculatePrimes = CalcPrimes(fn, sn);
 
+            //The conveintion in C# is to start body expressions in a seperate line and with bracets "{ }", even for oneliners
             foreach (var prime in calculatePrimes)
                 Console.WriteLine(prime);
         }
